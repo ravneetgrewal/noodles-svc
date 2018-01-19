04a1bd54054bf1d4b9fc46f3cc0365cbe9802cc9
Ravneet Grewal <rgrewal@undertone.com>
2018-01-19 13:38:09 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+    var dateTime = Date.now();
+    res.json({MyNoodles: "Noodles are timstamped - " + dateTime});
+});
+
+module.exports = router;
